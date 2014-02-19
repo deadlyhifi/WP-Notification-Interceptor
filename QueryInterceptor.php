65de85205f84e820196eed22de5de2318af60df0
Tom de Bruin <tom@deadlyhifi.com>
2014-02-19 07:59:07 +0000

    I finally invent something that works

---
+++ QueryInterceptor.php
@@ -0,0 +1,44 @@
+<?php
+
+abstract class QueryInterceptor {
+
+	abstract public function intercept($query);
+
+	public final function __construct()
+	{
+		if(!isset($this->path))
+			throw new Exception(__CLASS__ . ' must have a $path variable defined.');
+
+		add_filter('query_vars', [$this, 'add_query_vars']);
+		add_filter('rewrite_rules_array', [$this, 'add_rewrite_rules']);
+		add_action('template_redirect', [$this, 'template_redirect']);
+	}
+
+	// Add the URL
+	public final function add_query_vars($vars)
+	{
+	    $vars[] = $this->path;
+		return $vars;
+	}
+
+	// Add the new rewrite rule to existing ones.
+	public final function add_rewrite_rules($rules)
+	{
+	    $new_rules = [$this->path . '/([^/]+)/?$' => 'index.php?' . $this->path . '=$matches[1]'];
+	    $rules = $new_rules + $rules;
+
+	    return $rules;
+	}
+
+	// fire function that will act upon url query.
+	public final function template_redirect()
+	{
+		global $wp_query;
+
+		if(isset($wp_query->query_vars[$this->path])) {
+			$this->intercept(get_query_var($this->path));
+			exit();
+		}
+	}
+
+}
